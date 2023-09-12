bf718b78dc6079fec58b16e4174738abf536a9b5
ismail <ismailbary2@gmail.com>
2023-09-12 20:39:30 +0800

    add overflow

--- src/containers/Identity/index.jsx
+++ src/containers/Identity/index.jsx
@@ -50,7 +50,7 @@ function Identity (){
                     </div>
            </Col>
            <Col className="col-9 " style={{height: '100vh'}}>
-           <Card className="border-0 bg-success p-2 bg-opacity-10" style={{width: '90%'}}>
+           <Card className="border-0 bg-success p-2 bg-opacity-10 overflow-x" style={{width: '90%'}}>
             <div>
             <BiSolidUserCircle className="text-primary" style={{borderRadius: '50%', width: '30px', height: '30px'}} src={avatar} alt="" />
             <span className="fw-bold">DATA PRIBADI</span>
@@ -59,27 +59,27 @@ function Identity (){
                     <Col className="col-5">
                         <div>
                             <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                            <span className="ms-2 " style={{fontSize: '10px'}}>Nama</span>
+                            <span className="ms-2 font-card" >Nama</span>
                         </div>
                         <div>
                             <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                            <span className="ms-2 " style={{fontSize: '10px'}}>Tempat & Tanggal Lahir</span>
+                            <span className="ms-2 font-card" >Tempat & Tanggal Lahir</span>
                         </div>
                         <div>
                             <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                            <span className="ms-2 " style={{fontSize: '10px'}}>Suku</span>
+                            <span className="ms-2 font-card" >Suku</span>
                         </div>
                         <div>
                             <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                            <span className="ms-2 " style={{fontSize: '10px'}}>Golongan darah</span>
+                            <span className="ms-2 font-card" >Golongan darah</span>
                         </div>
                         <div>
                             <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                            <span className="ms-2 " style={{fontSize: '10px'}}>Berat badan</span>
+                            <span className="ms-2 font-card" >Berat badan</span>
                         </div>
                         <div>
                             <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                            <span className="ms-2 " style={{fontSize: '10px'}}>Agama</span>
+                            <span className="ms-2 font-card" >Agama</span>
                         </div>
                     </Col>
                     <Col>
@@ -105,7 +105,7 @@ function Identity (){
                     </Col>
                 </Row>    
             </Card>
-            <Card className="mt-4 border-0 bg-success p-2 bg-opacity-10" style={{width: '90%'}}>
+            <Card className="mt-4 border-0 bg-success p-2 bg-opacity-10 overflow-x" style={{width: '90%'}}>
                 <div>
                     <BiSolidUserCircle className="text-primary" style={{borderRadius: '50%', width: '30px', height: '30px'}} src={avatar} alt="" />
                     <span className="fw-bold">PENDIDIKAN FORMAL</span>
@@ -113,7 +113,7 @@ function Identity (){
                 <Row className="ms-1">
                     <div>
                         <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                        <span className="ms-2 " style={{fontSize: '10px'}}>
+                        <span className="ms-2 font-card" >
                             <span className="fw-bold me-3">SDN 234 Barambang II</span>
                             2002 - 2008
                             
@@ -127,7 +127,7 @@ function Identity (){
                 <Row className="ms-1">
                     <div className="mt-3">
                         <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                        <span className="ms-2 " style={{fontSize: '10px'}}>
+                        <span className="ms-2 font-card" >
                             <span className="fw-bold me-3">SMPN 16 Mandai :</span>
                             2008 - 2011
                             
@@ -141,7 +141,7 @@ function Identity (){
                 <Row className="ms-1">
                 <div className="mt-3">
                     <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                    <span className="ms-2 " style={{fontSize: '10px'}}>
+                    <span className="ms-2 font-card" >
                         <span className="fw-bold me-3">SMAN 8 Mandai-Maros : </span>
                          2012 - 2014
                          
@@ -155,7 +155,7 @@ function Identity (){
             <Row className="ms-1">
                 <div className="mt-3">
                     <AiFillCheckCircle style={{fontSize: '15px'}}/>
-                    <span className="ms-2 " style={{fontSize: '10px'}}>
+                    <span className="ms-2 font-card" >
                         <span className="fw-bold me-3">Universitas Muslim Maros</span>
                          2015 - 2019
                          
@@ -167,7 +167,7 @@ function Identity (){
                 </div>
             </Row>        
             </Card>
-            <Card className="mt-3 border-0 bg-success p-2 bg-opacity-10" style={{width: '90%'}}>
+            <Card className="mt-3 border-0 bg-success p-2 bg-opacity-10 overflow-x" style={{width: '90%'}}>
             <div>
             <BiSolidUserCircle className="text-primary" style={{borderRadius: '50%', width: '30px', height: '30px'}} src={avatar} alt="" />
             <span className="fw-bold">TENTANG SAYA</span>
@@ -176,44 +176,44 @@ function Identity (){
                 <table className="ms-3" style={{fontSize: '10px', width: '90%'}} >
                     <tbody>
                         <tr>
-                            <td className="fw-bold">Warna Favorit</td>
-                            <td>: Biru, Hijau, Coklat</td>
+                            <td className="fw-bold font-card">Warna Favorit</td>
+                            <td className="font-card">: Biru, Hijau, Coklat</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold">Makanan Favorit</td>
-                            <td>: Ayam, Sayur, Kentang, salad</td>
+                            <td className="fw-bold font-card">Makanan Favorit</td>
+                            <td className="font-card">: Ayam, Sayur, Kentang, salad</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold"> Minuman Favorit </td>
-                            <td >Madu, air Kelapa, Jus Melon, Jus Buah Naga</td>
+                            <td className="fw-bold font-card"> Minuman Favorit </td>
+                            <td className="font-card">Madu, air Kelapa, Jus Melon, Jus Buah Naga</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold"> Hal yang Disuka </td>
-                            <td >: Bangun Pagi, Membaca, Belajar Bahasa Pemrograman web</td>
+                            <td className="fw-bold font-card"> Hal yang Disuka </td>
+                            <td className="font-card">: Bangun Pagi, Membaca, Belajar Bahasa Pemrograman web</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold"> Hal yang Tidak Disuka </td>
-                            <td >: Rokok</td>
+                            <td className="fw-bold font-card"> Hal yang Tidak Disuka </td>
+                            <td className="font-card">: Rokok</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold"> Karakter + </td>
-                            <td >: Rapi, Semangat</td>
+                            <td className="fw-bold font-card"> Karakter + </td>
+                            <td className="font-card">: Rapi, Semangat</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold"> Karakter - </td>
-                            <td >: Tidak Tegas, Pelupa, Tidak Teliti</td>
+                            <td className="fw-bold font-card"> Karakter - </td>
+                            <td className="font-card">: Tidak Tegas, Pelupa, Tidak Teliti</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold"> Riwayat Penyakit </td>
-                            <td >: -</td>
+                            <td className="fw-bold font-card"> Riwayat Penyakit </td>
+                            <td className="font-card">: -</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold"> Keterangan </td>
-                            <td >: Aktif Bekerja di Lingkungan Rumah tahanan Negara Kelas IIB Bantaeng</td>
+                            <td className="fw-bold font-card"> Keterangan </td>
+                            <td className="font-card">: Aktif Bekerja di Lingkungan Rumah tahanan Negara Kelas IIB Bantaeng</td>
                         </tr>
                         <tr>
-                            <td className="fw-bold"> Tarbiah </td>
-                            <td >: Kajian Rutin ( ust. Nasrum, S.Pd.I)</td>
+                            <td className="fw-bold font-card"> Tarbiah </td>
+                            <td className="font-card">: Kajian Rutin ( ust. Nasrum, S.Pd.I)</td>
                         </tr>
                     </tbody>
                     </table>
