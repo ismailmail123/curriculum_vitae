f76fe6a587b655efc3ae31aca322fe10ab5d3ebd
ismail <ismailbary2@gmail.com>
2023-09-13 20:31:59 +0800

    add file downloader

--- src/containers/Unduh/index.jsx
+++ src/containers/Unduh/index.jsx
@@ -5,7 +5,7 @@ function Unduh (){
     return(
         <div className='d-flex justify-content-center align-items-center ' style={{width: '100%', height: '100vh'}}>
             <div>
-                <h4 className='text-primary'>Silahkan kloik tombol <a href='https://drive.google.com/file/d/16UMp3lLjDw3yb5CSDAt7YfjqzvyTKF5d/view?usp=drive_link'>
+                <h4 className='text-primary'>Silahkan klik tombol <a href='https://drive.google.com/file/d/16UMp3lLjDw3yb5CSDAt7YfjqzvyTKF5d/view?usp=drive_link'>
             <BiSolidDownload className="fs-1 text-success text-center" /></a> </h4>
             </div>
         </div>
