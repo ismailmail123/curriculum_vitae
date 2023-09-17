b2299b2d8f89a742a13847faf33c5ba7c840c7fc
ismail <ismailbary2@gmail.com>
2023-09-18 00:38:38 +0800

    update emoticon

--- src/containers/Message/index.jsx
+++ src/containers/Message/index.jsx
@@ -79,7 +79,7 @@ function Message (){
                             <div key={messageId} className=' d-flex justify-content-center'>
                                 <Card className="mt-3 d-flex" style={{width: '350px', backgroundColor: '#03fc7b' }} >
                                 <div className="d-flex justify-content-between">
-                                    <span className="fw-bold fs-6 lh-1 ms-2 text-light">👨‍💼{recipient.name}</span>
+                                    <span className="fw-bold fs-6 lh-1 ms-2 text-light">😊{recipient.name}</span>
                                     <span className="text-end text-secondary" style={{fontSize: '10px'}}>{dayjs(recipient?.createdAt).format("DD MMMM, HH:mm")}</span>
                                 </div>
                                
